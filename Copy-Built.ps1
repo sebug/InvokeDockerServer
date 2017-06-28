3f1b1a0f2c040eb0c3209aecf6ea2e18c0c3425a
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2017-06-28 18:37:21 +0000

    Initial version of copying stuff over

---
+++ Copy-Built.ps1
@@ -0,0 +1,11 @@
+﻿function Copy-Built {
+    param (
+        $targetIP
+    )
+
+    winrm s winrm/config/client "@{TrustedHosts=""$targetIP""}"
+    $cs = New-PSSession -ComputerName $targetIP -Name KubeDev
+    $tp = Invoke-Command -Session $cs -ScriptBlock { if (!(Test-Path C:\Dockerbuilds)) { mkdir C:\Dockerbuilds } }
+    Copy-Item -Recurse C:\Sources\TalkNotesBack -Destination C:\Dockerbuilds\TalkNotesBack -ToSession $cs
+    Copy-Item -Recurse C:\Sources\TalkNotesFront -Destination C:\Dockerbuilds\TalkNotesFron -ToSession $cs
+}
