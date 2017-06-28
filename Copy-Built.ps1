c4d7b9ae6197fd3be95c81f52fce89966b71a9f4
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2017-06-28 18:40:26 +0000

    Typo

--- Copy-Built.ps1
+++ Copy-Built.ps1
@@ -7,5 +7,5 @@
     $cs = New-PSSession -ComputerName $targetIP -Name KubeDev
     $tp = Invoke-Command -Session $cs -ScriptBlock { if (!(Test-Path C:\Dockerbuilds)) { mkdir C:\Dockerbuilds } }
     Copy-Item -Recurse C:\Sources\TalkNotesBack -Destination C:\Dockerbuilds\TalkNotesBack -ToSession $cs
-    Copy-Item -Recurse C:\Sources\TalkNotesFront -Destination C:\Dockerbuilds\TalkNotesFron -ToSession $cs
+    Copy-Item -Recurse C:\Sources\TalkNotesFront -Destination C:\Dockerbuilds\TalkNotesFront -ToSession $cs
 }
