cd932beb8dfe0955a97228974c6e970f8a97350e
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2017-06-28 20:07:49 +0000

    aand build frontend

---
+++ Build-Frontend.ps1
@@ -0,0 +1,4 @@
+﻿function Build-Frontend {
+    $msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe'
+    & $msbuild "C:\Sources\TalkNotesFront\TalkNotesFront.sln" /property:Configuration=Release /property:Platform="Any CPU" /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
+}
