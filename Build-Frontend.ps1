b968c9f09ad81c6e9eb4bc3cbafeec1b526a7322
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2017-06-30 06:02:09 +0000

    More complete build-frontend

--- Build-Frontend.ps1
+++ Build-Frontend.ps1
@@ -1,4 +1,8 @@
 ﻿function Build-Frontend {
+    $path = pwd
+    cd C:\Sources\TalkNotesFront
+    C:\Sources\nuget.exe restore
     $msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe'
     & $msbuild "C:\Sources\TalkNotesFront\TalkNotesFront.sln" /property:Configuration=Release /property:Platform="Any CPU" /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
+    cd $path
 }
